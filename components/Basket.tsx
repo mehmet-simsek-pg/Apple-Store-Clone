803361b51441ffff72b677a55ae94a1723dbee64
Mehmet Simsek <simsekmehmet00@gmail.com>
2023-01-23 17:05:00 +0100

    update basket style

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -10,11 +10,9 @@ function Basket() {
 
   return (
     <Link href="/checkout">
-      <div className="fixed bottom-10 right-10 z-50 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-gray-300">
+      <div className="app__basket-wrapper">
         {items.length > 0 && (
-          <span className="absolute -right-2 -top-2 z-50 flex h-7 w-7 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
-            {items.length}
-          </span>
+          <span className="app__basket-count">{items.length}</span>
         )}
         <ShoppingBagIcon className="headerIcon h-8 w-8" />
       </div>
